4d64cafbaa38e8b78ad8e3f03be1512cb17b045c
Ambiente 106 <ambiente106@iMac-de-CADPH.local>
2026-03-12 14:23:31 -0500

    login

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    DISCAPACIDAD_CHOICES = [
+        ('ninguna', 'Ninguna'),
+        ('sordo',   'Sordo'),
+        ('mudo',    'Mudo'),
+    ]
+
+    discapacidad = models.CharField(
+        max_length=10,
+        choices=DISCAPACIDAD_CHOICES,
+        default='ninguna'
+    )
+
+    def __str__(self):
+        return self.username
