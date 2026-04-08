a8ed7657fa851690a2634b85f500aa38496fec50
Jefferson Tapiero <jtapieromalambo@gmail.com>
2026-04-08 17:00:13 -0500

    colores de admin y svg y optimizacion de archivos

--- reconocimientos/apps.py
+++ reconocimientos/apps.py
@@ -2,10 +2,6 @@ from django.apps import AppConfig
 
 
 class ReconocimientosConfig(AppConfig):
-    name = 'reconocimientos'
-
-
-class ReconocimientosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reconocimientos'
 
