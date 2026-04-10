84a7fb8dd5ce15d59116da8c75730b7407d26440
Crack-Dev-Web <crckyahir@gmail.com>
2026-04-10 15:15:36 -0500

    Ya se arreglo lo de la cuenta de eliminar y continuar con google

--- usuarios/adapters.py
+++ usuarios/adapters.py
@@ -1,20 +1,7 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from allauth.exceptions import ImmediateHttpResponse
-from django.shortcuts import redirect
-from django.contrib import messages
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def pre_social_login(self, request, sociallogin):
-        email = sociallogin.account.extra_data.get('email', '')
-        
-        if email:
-            user = User.objects.filter(email=email).first()
-            if user and user.is_deleted:
-                messages.error(
-                    request,
-                    'Esta cuenta fue eliminada. Debes crear una cuenta nueva para ingresar.'
-                )
-                raise ImmediateHttpResponse(redirect('registro'))
+    pass
