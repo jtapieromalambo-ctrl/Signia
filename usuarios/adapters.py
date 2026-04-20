861c3c85d05d814580f64c6d9af983d3eaa56446
Oscar Vargas <vargasoscarjulian595@gmail.com>
2026-04-20 13:43:17 -0500

    cambio de inactividad

--- usuarios/adapters.py
+++ usuarios/adapters.py
@@ -1,7 +1,8 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    pass
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+        user.is_active = True
+        user.save()
+        return user
